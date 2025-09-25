b9da0c761dde99314c55443ce8fd0967f292a28f
KermitXeno <elamreece@gmail.com>
2025-09-25 18:10:39 -0400

    made folder structure happy with the csv format,added empty py file for mri training

---
+++ ModelTraining/TrainMRI.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 25 16:05:50 2025
+
+@author: elamr
+"""
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+import matplotlib.pyplot as plt
