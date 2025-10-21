98d6832e91c0d30cf0c8fef87392dc8db0d6cd05
KermitXeno <elamreece@gmail.com>
2025-10-20 22:26:49 -0400

    No longer working on life style model, got file created for EEG model and soon to start the Bio model

---
+++ ModelTraining/EEG/TrainEEG.py
@@ -0,0 +1,12 @@
+"""
+@author: elamr
+"""
+#LSM model training utils
+from keras.src.optimizers import optimizer
+import tensorflow as tf
+import keras
+import pandas as pd
+
+#Model to process EEG data and train model eventually
+
+
