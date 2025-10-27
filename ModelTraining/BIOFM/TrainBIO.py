f5d7074937f2bebd89a683de8a4df141bdd376f8
GaryE1am <grelam@uncg.edu>
2025-10-27 13:49:32 -0400

    Add attempt to process data

--- ModelTraining/BIOFM/TrainBIO.py
+++ ModelTraining/BIOFM/TrainBIO.py
@@ -4,17 +4,37 @@
 """
 import os
 import pandas as pd
-import kaggle
-from kaggle.api.kaggle_api_extended import KaggleApi
-api = KaggleApi()
-api.authenticate()
-path = api.dataset_download_files("jboysen/mri-and-alzheimers", path='./ModelTraining/BIOFM/data/', unzip=True)
+# kaggle
+#from kaggle.api.kaggle_api_extended import KaggleApi
+#api = KaggleApi()
+#api.authenticate()
+#path = api.dataset_download_files("jboysen/mri-and-alzheimers", path='./ModelTraining/BIOFM/data/', unzip=True)
 print("Done downloading dataset")
 
-df = pd.read_csv("datasets\oasis_crosssectional.csv", header=None)
+df = pd.read_csv("./ModelTraining/BIOFM/data/oasis_longitudinal.csv", header=0)
+#print head of df
+print(df.head())
 
-df = df.drop(columns=['ID', 'Delay', 'Dominant_Hand'])
-df = df.replace({'Sex' : {'F':1, 'M':0}})
-df = df.convert_dtypes()
+df.drop('Subject ID')
+df.drop ('MR Delay')
+df.drop ('Hand')
+df.drop ('MRI ID')
+df.drop ('Group')
+df.drop ('Visit')
+df.replace({'M/F' : {'F':1, 'M':0}})
+df.dropna()
+df.convert_dtypes()
+
+def change_cdr(cdr):
+    if cdr == 0.5:
+        return 1
+    elif cdr == 1:
+        return 2
+    elif cdr == 2:
+        return 3
+    else:
+        return 0
+
+df['CDR'] = df['CDR'].map(change_cdr)
 
 df.to_csv('./ModelTraining/BIOFM/data/clean_oasis.csv', index=False,  header=False)
