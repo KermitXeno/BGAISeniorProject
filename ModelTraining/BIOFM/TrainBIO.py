cb9435433dd01aa3bebc350eceb3c312850d4971
KermitXeno <elamreece@gmail.com>
2025-11-02 15:35:24 -0500

    finish bio factor model, best we are gonna get is 59 percent

--- ModelTraining/BIOFM/TrainBIO.py
+++ ModelTraining/BIOFM/TrainBIO.py
@@ -3,11 +3,15 @@
 @author: elamr
 """
 import os
+from keras.src.layers import Activation
 import pandas as pd
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import models
-from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.layers import Dense, Dropout, BatchNormalization
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.utils import to_categorical
+import numpy as np
 # kaggle
 #from kaggle.api.kaggle_api_extended import KaggleApi
 #api = KaggleApi()
@@ -16,8 +20,9 @@ from tensorflow.keras.layers import Dense, Dropout
 print("Done downloading dataset")
 
 df = pd.read_csv("./ModelTraining/BIOFM/data/oasis_longitudinal.csv", header=0)
-#print head of df
+
 print(df.head())
+print(df.dtypes)
 
 df.drop('Subject ID', axis=1, inplace=True)
 df.drop('MR Delay', axis=1, inplace=True)
@@ -26,31 +31,51 @@ df.drop('MRI ID', axis=1, inplace=True)
 df.drop('Group', axis=1, inplace=True)
 df.drop('Visit', axis=1, inplace=True)
 df['M/F'] = df['M/F'].map({'F':0, 'M':1})
-df['CDR'] = df['CDR'].map({0.0:0, 0.5:1, 1.0:2, 2.0:3})
+
+def map_cdr(value):
+    if value == 0.0:
+        return 0
+    elif value == 0.5:
+        return 1
+    elif value == 1.0:
+        return 2
+    elif value == 2.0:
+        return 3
+    else:
+        return -1
+df['CDR'] = df['CDR'].apply(map_cdr)
+
 df = df.dropna()
 df.convert_dtypes()
 
 print(df.head())
 df.to_csv('./ModelTraining/BIOFM/data/clean_oasis.csv', index=False,  header=False)
 
-labels = df['CDR']
+labels = to_categorical(df['CDR'], num_classes=4)
 df.drop('CDR', axis=1, inplace=True)
-print(labels.head())
 
 model = tf.keras.Sequential()
-model.add(Dense(64, activation='relu', input_shape=(df.shape[1],)))
+model.add(Dense(512, activation='sigmoid', input_shape=(8,)))
+model.add(Dense(256, activation='sigmoid'))
+model.add(Dense(128, activation='sigmoid'))
+model.add(Dense(64, activation='sigmoid'))
+model.add(BatchNormalization())
+model.add(Dense(32, activation='softmax'))
 model.add(Dropout(0.05))
-model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='softmax'))
 model.add(Dropout(0.05))
-model.add(Dense(32, activation='relu'))
+model.add(Dense(32, activation='softmax'))
 model.add(Dropout(0.05))
-model.add(Dense(32, activation='relu'))
+model.add(Dense(32, activation='softmax'))
 model.add(Dropout(0.05))
-model.add(Dense(4, activation='softmax'))
+model.add(Dense(4))
 
-optimizer = tf.keras.optimizers.SGD(learning_rate=0.0008)
-model.compile(optimizer=optimizer, loss='poisson', metrics=['accuracy'])
-model.fit(df, labels, epochs=64, batch_size=32, validation_split=0.2)
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
+loser = keras.losses.BinaryCrossentropy(from_logits=True)
+model.compile(optimizer=optimizer, loss=loser, metrics=['accuracy'])
+model.fit(x=df, y=labels, epochs=64, batch_size=32)
+
+model.save("./ModelTraining/BIOFM/weights/BIOFMGENETV1.keras")
 
 
 
